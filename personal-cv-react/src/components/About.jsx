0afc3d4d69ee6824d669eb0804e6257d62a591e0
digangjaysonrey123 <digang.jaysonrey123@gmail.com>
2026-03-05 15:18:22 +0800

    WSAT-M6: Enhanced React CV with props and dynamic rendering

--- personal-cv-react/src/components/About.jsx
+++ personal-cv-react/src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div className="container">
       <Card title="About Me">
         <p>
-          Hello! I am Jayson Rey Digang, a 21 years old IT student at the University of Science and 
+          Hello! I am Jayson Rey Digang, a 22 years old IT student at the University of Science and 
           Technology of Southern Philippines (USTP). I originally come from Purok 11 Gusa, Cagayan de Oro City, 
           but I currently live with my aunt at Camaman-an, Cagayan de Oro City.
         </p>
