8a97b9a1f5319467d12ae5d67d303583d7318fb5
maicode10 <domaog.mairalorraine@gmail.com>
2026-04-16 15:40:03 +0800

    WSAT-M8: Connected PHP API to MySQL Database

--- personal-cv-react/src/App.jsx
+++ personal-cv-react/src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Education from './components/Education';
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 const skills = [
   "HTML",
@@ -55,6 +56,7 @@ function App() {
       <Skills skills={skills} />
       <Education education={education} />
       <Contact />
+      <ContactList />
     </div>
   );
 }
